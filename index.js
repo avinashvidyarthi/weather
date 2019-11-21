102eaa429ff4c3244a8ad08ff7c4512c02fb9477
avinashvidyarthi <avinashvidyaryhi@gmail.com>
2019-11-22 01:18:59 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,56 @@
+$(function () {
+    //console.log("ready");
+});
+
+
+// --------------fetch_data() function---------------------//
+function fetch_data(){
+    if (navigator.geolocation) {
+        //console.log("geolocation active");
+        let x = navigator.geolocation.getCurrentPosition(ongetPos, errors);
+    } else {
+        console.log("geoloaction not active");
+    }
+}
+
+
+// -----------handler function-----------//
+function ongetPos(pos) {
+    $.ajax({
+        type: "get",
+        url: "https://api.openweathermap.org/data/2.5/weather?lat="+pos.coords.latitude+"&lon="+pos.coords.longitude+"&appid=6b30ae17ca20cafe9ddccdb52f660c55&units=metric",
+        success: function (response) {
+            send_output(response); //setting response html
+        },
+        beforeSend: function(){
+            //console.log("before send");
+        }
+    });
+}
+
+function errors(err) {
+    console.log("lnglreng");
+    if (err.code == 0) {
+        alert("Unknown Error!");
+    }
+    else if (err.code == 1) {
+        alert("Permission Denied!");
+    }
+    else if (err.code == 2) {
+        alert("Position Unavailable");
+    }
+    else if (err.code == 3) {
+        alert("Timeout");
+    }
+}
+
+
+function send_output(response){
+    //console.log(response);
+    var out=$("#output");
+    var res="<div class='col-md-12'>";
+    res+="<h2>"+response.main.temp+" C</h2>";
+    res+="<h3>"+response.name+","+response.sys.country+"</h3>";
+    res+="</div>";
+    out.html(res);
+}
